bd4953f9416e00a40d05da597a7f554c5a2b62eb
AgborSuccess <agborsuccess2007@gmail.com>
2023-03-03 04:37:52 -0800

    Implemented Playing of sound In the Splash Screen

--- app/src/main/java/com/example/schedule/SplashActivity.java
+++ app/src/main/java/com/example/schedule/SplashActivity.java
@@ -3,6 +3,7 @@ package com.example.schedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -15,13 +16,31 @@ public class SplashActivity extends AppCompatActivity {
 
         final Intent intent = new Intent(SplashActivity.this, MainActivity.class);
 
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.splash_music);
+        mediaPlayer.start();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 500);
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                mediaPlayer.release();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 500);
+            }
+        });
     }
 
 }
